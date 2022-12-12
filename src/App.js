5686627858935ca4925873e5fd6d94edd17c2777
MatiBravo47 <matibravoneron@gmail.com>
2022-12-12 18:19:57 -0300

    add new features

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import Pages from "./pages/Pages";
+import Category from "./components/Category.jsx"
+import {BrowserRouter} from 'react-router-dom'
+
 function App() {
   return (
     <div className="App">
+      <BrowserRouter>
+      <Category/>
       <Pages/>
+      </BrowserRouter>
     </div>
   );
 }
